ec3ba39983aa23c52357374896ecc98170fe5deb
Alan Guzek <alan.guzek@swmansion.com>
2026-05-25 13:29:18 +0200

    alias module

--- lib/voyager/telemetry/handler/export.ex
+++ lib/voyager/telemetry/handler/export.ex
@@ -2,10 +2,11 @@ defmodule Voyager.Telemetry.Handler.Export do
   @moduledoc false
 
   alias Voyager.Telemetry.Parser
+  alias Voyager.Telemetry.Export
 
   @spec handle_event(String.t(), map(), map(), map()) :: :ok
   def handle_event(event, measurements, metadata, _config) do
-    Voyager.Telemetry.Export.send(%{
+    Export.send(%{
       event: Parser.parse_event(event),
       measurements: measurements,
       metadata: Parser.parse_metadata(event, metadata),
