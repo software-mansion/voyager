4849c267d00154d9621c36f8b43e3e02afb36233
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-05-26 12:38:26 +0200

    fix format alias

--- mix.exs
+++ mix.exs
@@ -63,7 +63,8 @@ defmodule Voyager.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
-      format: ["format", "cmd npm --prefix assets run format", "cmd npm --prefix e2e run format"],
+      format: ["format", "cmd npm --prefix assets run format"],
+      "format.all": ["format", "cmd npm --prefix e2e run format"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
