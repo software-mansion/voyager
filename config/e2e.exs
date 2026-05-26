c7f5b7c3ccd9088ea94b39c2fdab4128704cbd1a
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-05-26 12:00:04 +0200

    fix workflow

--- config/e2e.exs
+++ config/e2e.exs
@@ -6,8 +6,7 @@ config :voyager, Voyager.Repo,
 
 config :voyager, VoyagerWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4001],
-  secret_key_base: "e2e00000000000000000000000000000000000000000000000000000000000000000",
-  server: true
+  secret_key_base: "e2e00000000000000000000000000000000000000000000000000000000000000000"
 
 config :logger, level: :warning
 config :phoenix, :plug_init_mode, :runtime
