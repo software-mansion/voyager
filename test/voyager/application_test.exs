7b214150e87c3d1d5cacf6b5a5f9d3e434e08c54
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2026-05-27 12:58:44 +0200

    Replace fetching logic with mock

--- test/voyager/application_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Voyager.ApplicationTest do
-  use ExUnit.Case, async: true
-
-  test "Voyager.Inspector.TaskSupervisor is running" do
-    pid = Process.whereis(Voyager.TaskSupervisor)
-    assert is_pid(pid)
-    assert Process.alive?(pid)
-  end
-end
