28957e75d1394c67e6a7d26752130785493ef5f8
kraleppa <krzysztof.nalepa@swmansion.com>
2026-06-02 15:03:50 +0200

    Remove obsolete e2e test

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -44,13 +44,13 @@ export default defineConfig({
     {
       name: 'node chromium',
       use: { ...devices['Desktop Chrome'] },
-      testMatch: ['**/node_info.spec.ts', '**/recent_connections.spec.ts'],
+      testMatch: '**/recent_connections.spec.ts',
       dependencies: ['connect'],
     },
     {
       name: 'node firefox',
       use: { ...devices['Desktop Firefox'] },
-      testMatch: ['**/node_info.spec.ts', '**/recent_connections.spec.ts'],
+      testMatch: '**/recent_connections.spec.ts',
       dependencies: ['connect'],
     },
   ],
