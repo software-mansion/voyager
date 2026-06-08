cab06e1a4dcfacaa0168794ad1422a2ba4d7a301
kraleppa <krzysztof.nalepa@swmansion.com>
2026-06-08 13:58:30 +0200

    Replace pixels with tailwind class

--- lib/voyager_web/live/node_info_live.ex
+++ lib/voyager_web/live/node_info_live.ex
@@ -41,7 +41,7 @@ defmodule VoyagerWeb.NodeInfoLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="max-w-[1536px] mx-auto p-6 sm:p-8">
+    <div class="max-w-screen-2xl mx-auto p-6 sm:p-8">
       <header class="mb-8 flex flex-wrap items-center justify-between gap-4">
         <div>
           <h1 class="font-mono text-base-content text-2xl font-bold tracking-tight">
