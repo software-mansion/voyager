c52783ed2957829c2b13e349fbf0c1cc58e66cd2
Alan Guzek <alan.guzek@swmansion.com>
2026-06-29 16:54:21 +0200

    distribution settings changeset

---
+++ lib/voyager_web/form_schemas/distribution_settings.ex
@@ -0,0 +1,20 @@
+defmodule VoyagerWeb.FormSchemas.DistributionSettings do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  embedded_schema do
+    field :distribution_suffix, :string, default: ""
+  end
+
+  @spec changeset(map()) :: Ecto.Changeset.t()
+  def changeset(attrs \\ %{}) do
+    %__MODULE__{}
+    |> cast(attrs, [:distribution_suffix])
+    |> update_change(:distribution_suffix, &String.trim/1)
+    |> validate_format(:distribution_suffix, ~r/^[A-Za-z0-9_-]*$/,
+      message: "Use only letters, numbers, underscores, or hyphens"
+    )
+    |> validate_length(:distribution_suffix, max: 64)
+  end
+end
