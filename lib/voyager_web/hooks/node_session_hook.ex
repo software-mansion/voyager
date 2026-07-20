fd661a9e3cfdecfdd6ae13a9ceb42f6a3eb5eaa7
Hubert Kasprzycki <hubert.kasprzycki@swmansion.com>
2026-07-20 12:48:25 +0200

    Fix disconnected flash not showing when one window opened

--- lib/voyager_web/hooks/node_session_hook.ex
+++ lib/voyager_web/hooks/node_session_hook.ex
@@ -60,7 +60,7 @@ defmodule VoyagerWeb.Hooks.NodeSessionHook do
 
   defp handle_disconnect("disconnect", _params, socket) do
     Voyager.NodeSession.disconnect()
-    {:halt, redirect(socket, to: ~p"/")}
+    {:halt, socket}
   end
 
   defp handle_disconnect(_event, _params, socket), do: {:cont, socket}
