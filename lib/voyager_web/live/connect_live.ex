184bc3a967bbc3747b741ad505dabd6f42a64d66
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-07-30 16:36:59 +0200

    remove tailwind class

--- lib/voyager_web/live/connect_live.ex
+++ lib/voyager_web/live/connect_live.ex
@@ -47,13 +47,13 @@ defmodule VoyagerWeb.ConnectLive do
               />
               <p
                 :if={@mode == :direct}
-                class="font-mono text-base-content/70 text-md"
+                class="font-mono text-base-content/70"
               >
                 Inspect node on your machine.
               </p>
               <p
                 :if={@mode == :ssh}
-                class="font-mono text-base-content/70 text-md"
+                class="font-mono text-base-content/70"
               >
                 Tunnel into a remote machine to reach its node.
               </p>
