34563cbdccd6b4f88f41a1c769f0f8735550a6dd
Piotr Sączawa <piotrsaczawa3@gmail.com>
2026-07-31 14:45:17 +0200

    Change default window size on desktop

--- rel/app/src-tauri/src/lib.rs
+++ rel/app/src-tauri/src/lib.rs
@@ -76,7 +76,7 @@ fn create_window(app_handle: &tauri::AppHandle, port: u16) {
     let url = tauri::WebviewUrl::External(format!("http://127.0.0.1:{port}").parse().unwrap());
     tauri::WebviewWindowBuilder::new(app_handle, format!("window-{}", n), url)
         .title("Voyager")
-        .inner_size(800.0, 800.0)
+        .inner_size(1280.0, 800.0)
         .min_inner_size(800.0, 800.0)
         .build()
         .unwrap();
