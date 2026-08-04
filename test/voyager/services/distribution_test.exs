7e1a6e454fc70745e899bf582ce0a12350d1a306
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-08-04 14:15:33 +0200

    add tests

--- test/voyager/services/distribution_test.exs
+++ test/voyager/services/distribution_test.exs
@@ -1,5 +1,5 @@
 defmodule Voyager.Services.DistributionTest do
-  use ExUnit.Case, async: true
+  use Voyager.DataCase, async: false
 
   alias Voyager.Services.Distribution
 
@@ -15,6 +15,38 @@ defmodule Voyager.Services.DistributionTest do
     test "errors when there is no @ separator" do
       assert {:error, {:invalid_node_format, "noatsign"}} =
                Distribution.split_node_name("noatsign")
+    end
+  end
+
+  describe "ensure_distributed/1" do
+    setup do
+      on_exit(fn -> if Node.alive?(), do: :net_kernel.stop() end)
+    end
+
+    test "starts distribution with a :longnames node pinned to 127.0.0.1" do
+      assert :ok = Distribution.ensure_distributed(:longnames)
+
+      assert {:ok, "voyager", "127.0.0.1"} =
+               Node.self() |> Atom.to_string() |> Distribution.split_node_name()
+    end
+
+    test "starts distribution with a :shortnames node pinned to localhost" do
+      assert :ok = Distribution.ensure_distributed(:shortnames)
+
+      assert {:ok, "voyager", "localhost"} =
+               Node.self() |> Atom.to_string() |> Distribution.split_node_name()
+    end
+
+    test "restarts distribution under the new host when name_type changes" do
+      assert :ok = Distribution.ensure_distributed(:longnames)
+      assert :ok = Distribution.ensure_distributed(:shortnames)
+
+      assert {:ok, "voyager", "localhost"} =
+               Node.self() |> Atom.to_string() |> Distribution.split_node_name()
+    end
+
+    test "rejects an invalid name_type" do
+      assert {:error, :invalid_name_type} = Distribution.ensure_distributed(:bogus)
     end
   end
 end
