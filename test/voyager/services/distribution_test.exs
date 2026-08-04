a83829fa81234c45a5e1e3e760318d0f727fa4e2
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-08-04 14:34:29 +0200

    remove tests

--- test/voyager/services/distribution_test.exs
+++ test/voyager/services/distribution_test.exs
@@ -17,36 +17,4 @@ defmodule Voyager.Services.DistributionTest do
                Distribution.split_node_name("noatsign")
     end
   end
-
-  describe "ensure_distributed/1" do
-    setup do
-      on_exit(fn -> if Node.alive?(), do: :net_kernel.stop() end)
-    end
-
-    test "starts distribution with a :longnames node pinned to 127.0.0.1" do
-      assert :ok = Distribution.ensure_distributed(:longnames)
-
-      assert {:ok, "voyager", "127.0.0.1"} =
-               Node.self() |> Atom.to_string() |> Distribution.split_node_name()
-    end
-
-    test "starts distribution with a :shortnames node pinned to localhost" do
-      assert :ok = Distribution.ensure_distributed(:shortnames)
-
-      assert {:ok, "voyager", "localhost"} =
-               Node.self() |> Atom.to_string() |> Distribution.split_node_name()
-    end
-
-    test "restarts distribution under the new host when name_type changes" do
-      assert :ok = Distribution.ensure_distributed(:longnames)
-      assert :ok = Distribution.ensure_distributed(:shortnames)
-
-      assert {:ok, "voyager", "localhost"} =
-               Node.self() |> Atom.to_string() |> Distribution.split_node_name()
-    end
-
-    test "rejects an invalid name_type" do
-      assert {:error, :invalid_name_type} = Distribution.ensure_distributed(:invalid)
-    end
-  end
 end
