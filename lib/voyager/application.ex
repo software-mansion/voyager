807a3507ad74f35c57395d7e55f6660e1711e6d6
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-08-18 14:50:57 +0200

    add alias

--- lib/voyager/application.ex
+++ lib/voyager/application.ex
@@ -2,6 +2,7 @@ defmodule Voyager.Application do
   @moduledoc false
 
   use Application
+  alias Voyager.Services.Distribution
 
   @impl true
   def start(_type, _args) do
@@ -17,7 +18,7 @@ defmodule Voyager.Application do
       Voyager.Telemetry,
       {Phoenix.PubSub, name: Voyager.PubSub},
       {Task.Supervisor, name: Voyager.TaskSupervisor},
-      Supervisor.child_spec({Task, &Voyager.Services.Distribution.start_epmd/0}, id: :start_epmd),
+      Supervisor.child_spec({Task, &Distribution.start_epmd/0}, id: :start_epmd),
       Voyager.ProxyEpmd.TunnelRegistry,
       Voyager.NodeSession,
       {ElixirKit.PubSub, connect: elixirkit_pubsub || :ignore, on_exit: fn -> System.stop() end},
