390c00ce2554f8aa2a7deeb5409f1239fb1b92e3
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-08-20 14:08:29 +0200

    restore tests

--- test/voyager/services/distribution_test.exs
+++ test/voyager/services/distribution_test.exs
@@ -3,6 +3,21 @@ defmodule Voyager.Services.DistributionTest do
 
   alias Voyager.Services.Distribution
 
+  describe "split_node_name/1" do
+    test "splits name@host into its parts" do
+      assert {:ok, "myapp", "10.0.0.5"} = Distribution.split_node_name("myapp@10.0.0.5")
+    end
+
+    test "keeps the host intact when it contains an @-free remainder" do
+      assert {:ok, "myapp", "host@weird"} = Distribution.split_node_name("myapp@host@weird")
+    end
+
+    test "errors when there is no @ separator" do
+      assert {:error, {:invalid_node_format, "noatsign"}} =
+               Distribution.split_node_name("noatsign")
+    end
+  end
+
   describe "ensure_distributed/1" do
     setup do
       on_exit(fn -> if Node.alive?(), do: :net_kernel.stop() end)
