db601b131db38b37f4e33d60e61e468ffc7f5bbd
srzeszut <szczepan.rzeszutek@swmansion.com>
2026-09-10 16:11:13 +0200

    Release v0.2.0-rc.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Voyager.MixProject do
   def project do
     [
       app: :voyager,
-      version: "0.1.0",
+      version: "0.2.0-rc.0",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
