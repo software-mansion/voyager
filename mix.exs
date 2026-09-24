8c9df34ce3f28748c38ee8ba56b0a261d59169fb
Dawid Żak <dawid.zak@swmansion.com>
2026-09-24 12:21:44 +0200

    Release v0.2.0-rc.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Voyager.MixProject do
   def project do
     [
       app: :voyager,
-      version: "0.2.0-rc.0",
+      version: "0.2.0-rc.1",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
